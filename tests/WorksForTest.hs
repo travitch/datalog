5e987b9dc268897c203eafe37d21452c87f65ace
Tristan Ravitch <travitch@cs.wisc.edu>
2012-07-18 15:45:44 -0500

    Add a test (and it passes) for conditional clauses

--- tests/WorksForTest.hs
+++ tests/WorksForTest.hs
@@ -16,14 +16,15 @@ main = defaultMain tests
 tests :: [Test]
 tests = [ testGroup "t1" [ testCase "1" t1
                          , testCase "2" t2
-                         -- , testCase "3" t3
-                         -- , testCase "4" t4
+                         , testCase "3" t3
+                           -- , testCase "4" t4
                          ] ]
 
 data WorkInfo = EID !Int -- id
               | EN !Text -- Name
               | EP !Text -- Position
               | J !Text  -- Job
+              | EA !Int
               deriving (Eq, Ord, Show)
 
 instance Hashable WorkInfo where
@@ -31,20 +32,21 @@ instance Hashable WorkInfo where
   hash (EN n) = 2 `combine` hash n
   hash (EP p) = 3 `combine` hash p
   hash (J j) = 4 `combine` hash j
+  hash (EA a) = 5 `combine` hash a
 
 db1 :: Maybe (Database WorkInfo)
 db1 = makeDatabase $ do
-  employee <- addRelation "employee" 3
-  let emplFacts = [ [ EID 1, EN "Bob", EP "Boss" ]
-                  , [ EID 2, EN "Mary", EP "Chief Accountant"]
-                  , [ EID 3, EN "John", EP "Accountant" ]
-                  , [ EID 4, EN "Sameer", EP "Chief Programmer" ]
-                  , [ EID 5, EN "Lilian", EP "Programmer" ]
-                  , [ EID 6, EN "Li", EP "Technician" ]
-                  , [ EID 7, EN "Fred", EP "Sales" ]
-                  , [ EID 8, EN "Brenda", EP "Sales" ]
-                  , [ EID 9, EN "Miki", EP "Project Management" ]
-                  , [ EID 10, EN "Albert", EP "Technician" ]
+  employee <- addRelation "employee" 4
+  let emplFacts = [ [ EID 1, EN "Bob", EP "Boss", EA 51]
+                  , [ EID 2, EN "Mary", EP "Chief Accountant", EA 31]
+                  , [ EID 3, EN "John", EP "Accountant", EA 22 ]
+                  , [ EID 4, EN "Sameer", EP "Chief Programmer", EA 34 ]
+                  , [ EID 5, EN "Lilian", EP "Programmer", EA 24 ]
+                  , [ EID 6, EN "Li", EP "Technician", EA 40 ]
+                  , [ EID 7, EN "Fred", EP "Sales", EA 29 ]
+                  , [ EID 8, EN "Brenda", EP "Sales", EA 27 ]
+                  , [ EID 9, EN "Miki", EP "Project Management", EA 44 ]
+                  , [ EID 10, EN "Albert", EP "Technician", EA 23 ]
                   ]
   mapM_ (assertFact employee) emplFacts
 
@@ -93,8 +95,8 @@ q1 = do
       eid = LogicVar "E-ID"
       bid = LogicVar "B-ID"
   (worksFor, [x, y]) |- [ lit bossOf [bid, eid]
-                        , lit employee [eid, x, Anything]
-                        , lit employee [bid, y, Anything]
+                        , lit employee [eid, x, Anything, Anything]
+                        , lit employee [bid, y, Anything, Anything]
                         ]
   (worksFor, [x, y]) |- [ lit worksFor [x, z]
                         , lit worksFor [z, y]
@@ -123,4 +125,39 @@ t2 = do
   where
     expected = fromList [ [EN "Lilian", EN "Sameer"]
                         , [EN "Lilian", EN "Bob"]
+                        ]
+
+q2 = do
+  employee <- relationPredicateFromName "employee"
+  bossOf <- relationPredicateFromName "bossOf"
+  worksFor <- inferencePredicate "worksFor"
+  worksForYoung <- inferencePredicate "worksForYoung"
+  let x = LogicVar "X"
+      y = LogicVar "Y"
+      z = LogicVar "Z"
+      age = LogicVar "Age"
+      eid = LogicVar "E-ID"
+      bid = LogicVar "B-ID"
+  (worksFor, [x, y]) |- [ lit bossOf [bid, eid]
+                        , lit employee [eid, x, Anything, Anything]
+                        , lit employee [bid, y, Anything, Anything]
+                        ]
+  (worksFor, [x, y]) |- [ lit worksFor [x, z]
+                        , lit worksFor [z, y]
+                        ]
+  (worksForYoung, [x, y]) |- [ lit worksFor [x, y]
+                             , lit employee [eid, y, Anything, age]
+                             , cond1 (\(EA a) -> a < 49) age
+                             ]
+  issueQuery worksForYoung [ BindVar "name", y ]
+
+t3 :: Assertion
+t3 = do
+  let Just db = db1
+      Just qp = buildQueryPlan db q2
+
+  res <- executeQueryPlan qp db [("name", EN "Lilian")]
+  assertEqual "t1" expected (fromList res)
+  where
+    expected = fromList [ [EN "Lilian", EN "Sameer"]
                         ]
