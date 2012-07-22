1b199292b56ef13c49ab9d2cfab819bce30a01a5
Tristan Ravitch <travitch@cs.wisc.edu>
2012-07-22 13:09:30 -0500

    Add a 5 queens test

--- tests/NQueens.hs
+++ tests/NQueens.hs
@@ -2,8 +2,8 @@
 module Main ( main ) where
 
 import Control.Monad ( forM_ )
-import Data.Set ( fromList )
-import Data.Text ( Text )
+import Data.List ( sort )
+import qualified Data.Set as S
 import Test.Framework ( defaultMain, testGroup, Test )
 import Test.Framework.Providers.HUnit
 import Test.HUnit hiding ( Test )
@@ -15,6 +15,7 @@ main = defaultMain tests
 
 tests :: [Test]
 tests = [ testGroup "t1" [ testCase "4queens" t4
+                         , testCase "5queens" t5
                          ] ]
 
 type Position = (Int, Int)
@@ -25,15 +26,29 @@ dbN n = makeDatabase $ do
   position <- addRelation "position" 1
   forM_ posTuples $ \(x, y) -> assertFact position [ (x, y) ]
 
+-- Note, the restrictions on x and y equality also imply that the
+-- position rule can't select the same position more than once in a
+-- solution
 posCanAttack :: Position -> Position -> Bool
 posCanAttack (x1, y1) (x2, y2) =
   x1 == x2 || y1 == y2 || (abs (x1 - x2) == abs (y1 - y2))
+
+unique :: [[Position]] -> [[Position]]
+unique = S.toList . S.fromList
+
+-- Return False if any position can attack any others
+noneCanAttack :: [Position] -> Bool
+noneCanAttack [] = True
+noneCanAttack [_] = True
+noneCanAttack (p:ps) = not (any (posCanAttack p) ps) && noneCanAttack ps
 
 t4 :: Assertion
 t4 = do
   db4 <- dbN 4
   res <- queryDatabase db4 q
-  assertEqual "t4" (fromList []) (fromList res)
+  let res' = unique $ map sort res
+  print res'
+  assertBool "t4" $ all noneCanAttack res' && length res' == 2
   where
     q = do
       position <- relationPredicateFromName "position"
@@ -48,7 +63,6 @@ t4 = do
           p2 = LogicVar "P2"
           p3 = LogicVar "P3"
           p4 = LogicVar "P4"
-          -- p5 = LogicVar "P5"
       queens <- inferencePredicate "queens"
       (queens, [p1, p2, p3, p4]) |- [ lit position [p1]
                                     , lit position [p2]
@@ -61,4 +75,45 @@ t4 = do
                                     , negLit canAttack [p2, p4]
                                     , negLit canAttack [p3, p4]
                                     ]
-      issueQuery queens [Atom (1, 1), p2, p3, p4]
+      issueQuery queens [p1, p2, p3, p4]
+
+t5 :: Assertion
+t5 = do
+  db5 <- dbN 5
+  res <- queryDatabase db5 q
+  let res' = unique $ map sort res
+  print res'
+  assertBool "t5" $ all noneCanAttack res' && length res' == 10
+  where
+    q = do
+      position <- relationPredicateFromName "position"
+      canAttack <- inferencePredicate "canAttack"
+      let x = LogicVar "X"
+          y = LogicVar "Y"
+      (canAttack, [x, y]) |- [ lit position [x]
+                             , lit position [y]
+                             , cond2 posCanAttack (x, y)
+                             ]
+      let p1 = LogicVar "P1"
+          p2 = LogicVar "P2"
+          p3 = LogicVar "P3"
+          p4 = LogicVar "P4"
+          p5 = LogicVar "P5"
+      queens <- inferencePredicate "queens"
+      (queens, [p1, p2, p3, p4, p5]) |- [ lit position [p1]
+                                        , lit position [p2]
+                                        , lit position [p3]
+                                        , lit position [p4]
+                                        , lit position [p5]
+                                        , negLit canAttack [p1, p2]
+                                        , negLit canAttack [p1, p3]
+                                        , negLit canAttack [p1, p4]
+                                        , negLit canAttack [p1, p5]
+                                        , negLit canAttack [p2, p3]
+                                        , negLit canAttack [p2, p4]
+                                        , negLit canAttack [p2, p5]
+                                        , negLit canAttack [p3, p4]
+                                        , negLit canAttack [p3, p5]
+                                        , negLit canAttack [p4, p5]
+                                        ]
+      issueQuery queens [p1, p2, p3, p4, p5]
