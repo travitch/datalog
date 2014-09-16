e7fe8d5e6f6a0081a9eaf40bb2b14d0fcce853b1
Tristan Ravitch <tristan@nochair.net>
2014-09-15 21:21:50 -0700

    Add a missing Show instance

--- src/Database/Datalog.hs
+++ src/Database/Datalog.hs
@@ -50,6 +50,7 @@ import Database.Datalog.Stratification
 
 -- | A fully-stratified query plan that is ready to be executed.
 data QueryPlan a = QueryPlan (Query a) [[Rule a]]
+                 deriving (Show)
 
 -- | This is a shortcut to build a query plan and execute in one step,
 -- with no bindings provided.  It doesn't make sense to have bindings
